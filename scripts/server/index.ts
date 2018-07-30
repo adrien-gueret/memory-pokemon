d6ca33c30cd34235475678d22c2d950a006f6fa0
Adrien Guéret <adrien.grt@gmail.com>
2018-07-30 20:00:02 +0200

    chore(heroku): use express to serve files

--- scripts/server/index.ts
+++ scripts/server/index.ts
@@ -1,5 +1,20 @@
+import * as express from 'express';
 import MemoryGameFactory from './MemoryGameFactory';
+
+declare const __dirname;
+declare const process;
 
 const gameFactory = new MemoryGameFactory();
 
 gameFactory.listen();
+
+const app = express();
+
+const distDir = __dirname + '/../../dist/';
+
+app.use(express.static(distDir));
+
+const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
+    console.log('Client now running on port', port);
+});
