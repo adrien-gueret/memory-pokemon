8203e1269a54bf282961cb145dee7e6410984cfd
Adrien Guéret <adrien.grt@gmail.com>
2018-07-31 19:46:14 +0200

    refact(server): remove express

--- scripts/server/index.ts
+++ scripts/server/index.ts
@@ -1,4 +1,3 @@
-import * as express from 'express';
 import MemoryGameFactory from './MemoryGameFactory';
 
 declare const __dirname;
@@ -7,14 +6,3 @@ declare const process;
 const gameFactory = new MemoryGameFactory();
 
 gameFactory.listen();
-
-const app = express();
-
-const distDir = __dirname + '/../../dist/';
-
-app.use(express.static(distDir));
-
-const server = app.listen(process.env.PORT || 8080, function () {
-    const port = server.address().port;
-    console.log('Client now running on port', port);
-});
