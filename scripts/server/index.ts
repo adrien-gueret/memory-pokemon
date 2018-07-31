502539142cf728c8073037996d9106c8baad31ef
Adrien Guéret <adrien.grt@gmail.com>
2018-07-31 23:30:10 +0200

    refactor(server): check origins

--- scripts/server/index.ts
+++ scripts/server/index.ts
@@ -6,3 +6,8 @@ declare const process;
 const gameFactory = new MemoryGameFactory();
 
 gameFactory.listen();
+
+(gameFactory as any).io.origins((origin, callback) => {
+    console.log('check origin', origin);
+    callback(null, true);
+});
